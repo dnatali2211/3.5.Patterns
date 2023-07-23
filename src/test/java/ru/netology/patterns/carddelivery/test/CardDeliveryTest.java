b63f81e2a25c2e0b67186d9f002e15c6d7404de2
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-07-23 11:32:04 +0300

    Added allure

--- src/test/java/ru/netology/patterns/carddelivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/patterns/carddelivery/test/CardDeliveryTest.java
@@ -1,6 +1,10 @@
 package ru.netology.patterns.carddelivery.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.patterns.carddelivery.data.DataGenerator;
 
@@ -11,6 +15,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     String city = DataGenerator.generateCity();
     String firstDate = DataGenerator.generateDate(4, "dd.MM.yyyy");
     String secondDate = DataGenerator.generateDate(7, "dd.MM.yyyy");
@@ -24,6 +38,31 @@ public class CardDeliveryTest {
         $("[data-test-id=city] input").setValue(city);
         $("[data-test-id=date] input").doubleClick().sendKeys(firstDate);
         $("[data-test-id=name] input").setValue(name);
+        $("[data-test-id=phone] input").setValue(phoneNumber);
+        $(withText("Успешно")).shouldBe(Condition.hidden);
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(40));
+        $("[data-test-id=success-notification]").shouldHave(Condition.text("Успешно!\n" +
+                "Встреча успешно запланирована на " + firstDate)).shouldBe(Condition.visible);
+        $("[data-test-id=date] input").doubleClick().sendKeys(secondDate);
+        $(".button").click();
+        $("[data-test-id=replan-notification]").shouldBe(Condition.visible, Duration.ofSeconds(40));
+        $("[data-test-id=replan-notification]").shouldHave(Condition.text(
+                "У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(Condition.visible, Duration.ofSeconds(40));
+        $("[data-test-id=replan-notification] button").click();
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(40));
+        $("[data-test-id=success-notification]").shouldHave(Condition.text("Успешно!\n" +
+                "Встреча успешно запланирована на " + secondDate)).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void testAllure() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys(firstDate);
+        $("[data-test-id=name] input").setValue("Пётр");
         $("[data-test-id=phone] input").setValue(phoneNumber);
         $(withText("Успешно")).shouldBe(Condition.hidden);
         $("[data-test-id=agreement]").click();
