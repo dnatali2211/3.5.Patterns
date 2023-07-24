3392bfe819e3ac1fee20985fc371e139366e083a
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-07-24 14:22:52 +0300

    Deleted failed test

--- src/test/java/ru/netology/patterns/carddelivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/patterns/carddelivery/test/CardDeliveryTest.java
@@ -56,28 +56,5 @@ public class CardDeliveryTest {
                 "Встреча успешно запланирована на " + secondDate)).shouldBe(Condition.visible);
     }
 
-    @Test
-    void testAllure() {
-        open("http://localhost:9999");
 
-        $("[data-test-id=city] input").setValue(city);
-        $("[data-test-id=date] input").doubleClick().sendKeys(firstDate);
-        $("[data-test-id=name] input").setValue("Пётр");
-        $("[data-test-id=phone] input").setValue(phoneNumber);
-        $(withText("Успешно")).shouldBe(Condition.hidden);
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(40));
-        $("[data-test-id=success-notification]").shouldHave(Condition.text("Успешно!\n" +
-                "Встреча успешно запланирована на " + firstDate)).shouldBe(Condition.visible);
-        $("[data-test-id=date] input").doubleClick().sendKeys(secondDate);
-        $(".button").click();
-        $("[data-test-id=replan-notification]").shouldBe(Condition.visible, Duration.ofSeconds(40));
-        $("[data-test-id=replan-notification]").shouldHave(Condition.text(
-                "У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(Condition.visible, Duration.ofSeconds(40));
-        $("[data-test-id=replan-notification] button").click();
-        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(40));
-        $("[data-test-id=success-notification]").shouldHave(Condition.text("Успешно!\n" +
-                "Встреча успешно запланирована на " + secondDate)).shouldBe(Condition.visible);
-    }
 }
